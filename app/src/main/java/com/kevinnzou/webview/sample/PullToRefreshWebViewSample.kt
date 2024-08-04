e466502aff4859745efca49b04dd57f0667b6e4c
Waheed KhAn <waheedkhan6446@gmail.com>
2024-08-04 13:23:45 +0500

    Page responsive and scroll issue fix in web view.

--- app/src/main/java/com/kevinnzou/webview/sample/PullToRefreshWebViewSample.kt
+++ app/src/main/java/com/kevinnzou/webview/sample/PullToRefreshWebViewSample.kt
@@ -7,13 +7,12 @@ import android.util.Log
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -28,7 +27,7 @@ import com.kevinnzou.webview.ui.theme.ComposewebviewTheme
 import kotlinx.coroutines.delay
 
 class PullToRefreshWebViewSample : ComponentActivity() {
-    val initialUrl = "https://github.com/KevinnZou/compose-webview"
+    private val initialUrl = "https://www.thelinehotel.com/wp-admin" //"https://github.com/KevinnZou/compose-webview"
 
     @OptIn(ExperimentalMaterial3Api::class)
     @SuppressLint("SetJavaScriptEnabled")
@@ -43,6 +42,7 @@ class PullToRefreshWebViewSample : ComponentActivity() {
                 }
 
                 var refreshing by remember { mutableStateOf(false) }
+                var contentHeight by remember { mutableIntStateOf(0) }
                 LaunchedEffect(refreshing) {
                     if (refreshing) {
                         navigator.reload()
@@ -55,33 +55,48 @@ class PullToRefreshWebViewSample : ComponentActivity() {
                     state = rememberPullToRefreshState(isRefreshing = refreshing),
                     onRefresh = { refreshing = true }
                 ) {
-                    Column(
-                        modifier = Modifier.verticalScroll(state = rememberScrollState())
-                    ) {
-                        // A custom WebViewClient and WebChromeClient can be provided via subclassing
-                        val webClient = remember {
-                            object : AccompanistWebViewClient() {
-                                override fun onPageStarted(
-                                    view: WebView,
-                                    url: String?,
-                                    favicon: Bitmap?
-                                ) {
-                                    super.onPageStarted(view, url, favicon)
-                                    Log.d("Accompanist WebView", "Page started loading for $url")
+                    LazyColumn {
+                        item {
+                            // A custom WebViewClient and WebChromeClient can be provided via subclassing
+                            val webClient = remember {
+                                object : AccompanistWebViewClient() {
+                                    override fun onPageStarted(
+                                        view: WebView,
+                                        url: String?,
+                                        favicon: Bitmap?
+                                    ) {
+                                        super.onPageStarted(view, url, favicon)
+                                        Log.d(
+                                            "Accompanist WebView",
+                                            "Page started loading for $url"
+                                        )
+                                    }
+                                    override fun onPageFinished(view: WebView, url: String?) {
+                                        super.onPageFinished(view, url)
+                                        contentHeight = view.contentHeight // Get the content height of the WebView
+                                        Log.d(
+                                            "onPageFinished",
+                                            "content Height $contentHeight"
+                                        )
+                                    }
                                 }
                             }
-                        }
 
-                        WebView(
-                            state = state,
-                            modifier = Modifier
-                                .fillMaxSize(),
-                            navigator = navigator,
-                            onCreated = { webView ->
-                                webView.settings.javaScriptEnabled = true
-                            },
-                            client = webClient
-                        )
+                            WebView(
+                                state = state,
+                                modifier = if (contentHeight < 1) {
+                                    Modifier.fillParentMaxSize()
+                                } else {
+                                    Modifier
+                                        .fillMaxSize()
+                                },
+                                navigator = navigator,
+                                onCreated = { webView ->
+                                    webView.settings.javaScriptEnabled = true
+                                },
+                                client = webClient
+                            )
+                        }
                     }
                 }
             }
